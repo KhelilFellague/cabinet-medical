3ac897df1e4e63483492c82018df018f2f24a6f7
assia9 <65179509+assia9@users.noreply.github.com>
2020-06-16 23:49:23 +0200

    Add files via upload

---
+++ src/Visite.java
@@ -0,0 +1,20 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Visite extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Visite() {
+		setBounds(317,0,898,783);
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("VISITES");
+		lblNewLabel.setBounds(50, 112, 140, 364);
+		add(lblNewLabel);
+		setVisible(true);
+
+	}
+
+}
